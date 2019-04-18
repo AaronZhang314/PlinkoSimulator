0efec26f1923cba3b00e1ab1fcc1984c8ef8420a
Kevin Wang <kevinwang2040@gmail.com>
2019-04-18 11:09:43 -0400

    Update plinko.py

--- plinko.py
+++ plinko.py
@@ -1,5 +1,4 @@
-import sys
-import pygame
+import sys, pygame, random, time
 
 
 pygame.init() #initializes the game
@@ -8,10 +7,11 @@ pygame.init() #initializes the game
 white = (255,255,255) #theese are RGB color things
 green = (0,255,0)
 blue = (0,0,255)
+red = (255, 0, 0)
 black= (0,0,0)
 
 
-screen = pygame.display.set_mode((600,800)) #game window dimensions
+screen = pygame.display.set_mode((1800, 1000)) #game window dimensions
 screen_rect=screen.get_rect()
 pygame.display.set_caption('Plinko')#game name
 clock=pygame.time.Clock() #chooses the FPS
@@ -22,28 +22,46 @@ gameExit=False #when to end game
 myfont=pygame.font.SysFont(None, 20)
 myfont2=pygame.font.SysFont(None, 32)
 
+block_x = 900
+block_y = 50
+block=pygame.draw.circle(screen, red, (block_x, block_y), 30)
 
-block=pygame.draw.rect(screen, white, (550, 297, 24,24))
-block_x = 290
-block_y = 10
-speed = 0
 move = False
+tick = 0
+moves = 8
+move_y = 5
+move_x = 5
+
 while not gameExit: #unless gameExit is true
+    print move, moves
+    tick += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #when i click the x button it exits the loop, therefore closing it
                 gameExit=True
+                
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         move = True
+        block_x = 900
+        block_y = 50
 
     if move:
-        speed+=0.1
+        if tick > 15:
+            tick = 0
+            choice = random.randint(1, 2)
+            if choice == 1:
+                move_x *= -1
+        block_y += move_y
+        block_x += move_x
 
-    block_y+=speed
+                
+                
+                
     screen.fill(white)#background color
-    pygame.draw.rect(screen, blue, (block_x,block_y,20,20))
+    block=pygame.draw.circle(screen, red, (block_x, block_y), 50)
     pygame.display.update() #updates the display
-    clock.tick(80) #frames per second
+    clock.tick(30) #frames per second
+    
 pygame.quit()
 quit()
