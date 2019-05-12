4ba34f3b02aab910f29ae746f897f53ca0959dee
Kevin Wang <kevinwang2040@gmail.com>
2019-05-12 16:22:23 -0400

    Add files via upload

--- plinko/plinko3.py
+++ plinko/plinko3.py
@@ -3,8 +3,6 @@ import sys, pygame, random, time
 
 pygame.init() #initializes the game
 
-f = open("trials.txt", "w+")
-
 white = (255,255,255) #theese are RGB color things
 green = (0,255,0)
 blue = (0,0,255)
@@ -12,7 +10,7 @@ red = (255, 0, 0)
 black= (0,0,0)
 
 board_x = 205
-board_y = 0
+board_y = 100
 board_width = 1400
 board_height = 700
 
@@ -25,6 +23,12 @@ board_pic = pygame.image.load("board.png").convert_alpha()
 board_pic = pygame.transform.scale(board_pic, (board_width, board_height))
 peg_pic = pygame.image.load("peg.png").convert_alpha()
 peg_pic = pygame.transform.scale(peg_pic, (25, 25))
+title_pic = pygame.image.load("Title.png").convert_alpha()
+column_pic = pygame.image.load("col.png").convert_alpha()
+column_pic = pygame.transform.scale(column_pic, (1200, 250))
+bg_pic = pygame.image.load("background.png").convert_alpha()
+bg_pic = pygame.transform.scale(bg_pic, (1800, 1000))
+
 gameExit=False #when to end game
 
 
@@ -32,8 +36,7 @@ myfont=pygame.font.SysFont(None, 20)
 myfont2=pygame.font.SysFont(None, 32)
 
 block_x = 870
-block_y = 50
-#vblock=pygame.draw.circle(screen, red, (block_x, block_y), 30)
+block_y = 150
 
 move = False
 tick = 0
@@ -41,36 +44,30 @@ moves = 8
 move_y = 4
 move_x = 3.8
 
-occ = [0,0,0,0,0,0,0,0]
-path = [2,2,2,2,2,2,2,2,2,2,1]
-k = 0
+options = [1,2,3,4,5,6,7,8]
 
-def trackCol():
-    global block_x
+points = [8,6,4,2,2,4,6,8]
+
+count = {0:0, 2:0, 4:0, 6:0, 8:0}
+
+def getCol():
+
     if block_x < 460:
-        occ[0] += 1
-        print "Col 1"
-    if block_x < 506:
-        occ[1] += 1
-        print "Col 2"
+        return 1
+    elif block_x < 506:
+        return 2
     elif block_x < 656:
-        occ[2] += 1
-        print "Col 3"
+        return 3
     elif block_x < 806:
-        occ[3] += 1
-        print "Col 4"
+        return 4
     elif block_x < 956:
-        occ[4] += 1
-        print "Col 5"
+        return 5
     elif block_x < 1106:
-        occ[5] += 1
-        print "Col 6"
+        return 6
     elif block_x < 1256:
-        occ[6] += 1
-        print "Col 7"
+        return 7
     else:
-        occ[7] += 1
-        print "Col 8"
+        return 8
 
 def reset():
     global block_x
@@ -79,14 +76,13 @@ def reset():
     global move
     move = True
     block_x = 870
-    block_y = 50
+    block_y = 150
     moves = 8
-    #print "hihihadf", block_x, block_y
 
 trials = 0
+
 while not gameExit: #unless gameExit is true
-    #print move, moves
-    #print block_x, block_y
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #when i click the x button it exits the loop, therefore closing it
                 gameExit=True
@@ -94,17 +90,12 @@ while not gameExit: #unless gameExit is true
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        move = True
-        block_x = 870
-        block_y = 50
-        moves = 8
+        reset()
 
     if move:
-        if (block_y - 50) % 80 == 0:
+        if (block_y - 150) % 80 == 0:
             moves -= 1
             choice = random.randint(1, 2)
-            #choice = path[k]
-            k += 1
             if choice == 1:
                 move_x *= -1
         block_y += move_y
@@ -112,42 +103,39 @@ while not gameExit: #unless gameExit is true
         
     if moves == 0:
         trials += 1
-        k = 0
         move = False
-        trackCol()
-        reset()
-        moves = 8
+        col = getCol()
+        random.shuffle(options)
+        c1 = options[0]
+        c2 = options[1]
+        if col == c1 or col == c2:
+            #print "Success:", c1, c2, col
+            point_now = points[col - 1]
+            count[point_now] += 1
+        else:
+            count[0] += 1
+
                 
-                
-                
-    screen.fill(white)#background color
-    #block=pygame.draw.circle(screen, red, (block_x, block_y), 50)
+    screen.blit(bg_pic, (0,0))
+    screen.blit(column_pic, (285, 740))
+    
     screen.blit(board_pic, (board_x, board_y))
     screen.blit(ball_pic, (block_x, block_y))
+    screen.blit(title_pic, (625,-30))
     hGap = 150
     vGap = 82
     middle = 881
     for i in range(7):
         for j in range(i+1):
             if (i%2==0):
-                screen.blit(peg_pic, (middle-(i/2*hGap)+(j*hGap),95+(i*vGap)))
+                screen.blit(peg_pic, (middle-(i/2*hGap)+(j*hGap),95+(i*vGap) + 100))
             else:
-                screen.blit(peg_pic, (middle-((i+1)/2*hGap)+(j*hGap)+(hGap/2), 95+(i*vGap)))
-                
-            #if i == 6:
-             #   print middle-((i+1)/2*hGap)+(j*hGap)+(hGap/2), 95 + (i * vGap)
+                screen.blit(peg_pic, (middle-((i+1)/2*hGap)+(j*hGap)+(hGap/2), 95+(i*vGap) + 100))
+
     
 
-    for i in range(9):
-        pygame.draw.circle(screen, black, (board_x + ((i + 1) * 140), board_y + board_height), 2)
-
-    if trials == 1000:
-        gameExit = True
+        
     pygame.display.update() #updates the display
     clock.tick(30) #frames per second
 
-for i in range(8):
-    f.write("Col %d : %d\r\n" % (i + 1, occ[i]))
-    
-f.close()
 pygame.quit()
